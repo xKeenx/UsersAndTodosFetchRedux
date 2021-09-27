00c3eaf44d7b5bbd7efe0faba8b95f7924f9f5bc
Горожанкин Владислав <gorozhankin@bsu.edu.ru>
2021-09-27 16:47:49 +0300

    Test React Redux + Redux Thunk

---
+++ src/store/action-creators/todo.ts
@@ -0,0 +1,24 @@
+import {Dispatch} from "react";
+import axios from "axios";
+import {TodoAction, TodoActionTypes} from "../../types/todo";
+
+export const fetchTodos = (page = 1 , limit = 10) => {
+    return async (dispatch:Dispatch<TodoAction>) => {
+        try {
+            dispatch({type:TodoActionTypes.FETCH_TODOS})
+            const response = await axios.get("https://jsonplaceholder.typicode.com/todos",{
+                params:{_page:page,_limit:limit}
+            })
+            setTimeout(()=>{
+            },500)
+            dispatch({type:TodoActionTypes.FETCH_TODOS_SUCCESS,payload:response.data})
+        } catch (e) {
+            dispatch({type:TodoActionTypes.FETCH_TODOS_ERROR,
+                payload:"Произошла ошибка при загрузке пользователей"})
+        }
+
+    }
+}
+export function setTodoPage(page:number):TodoAction {
+    return {type:TodoActionTypes.SET_TODOS_PAGE,payload:page}
+}
---
+++ src/store/action-creators/user.ts
@@ -0,0 +1,20 @@
+import {UserAction, UserActionTypes} from "../../types/user";
+import {Dispatch} from "react";
+import axios from "axios";
+
+export const fetchUsers = () => {
+    return async (dispatch:Dispatch<UserAction>) => {
+        try {
+            dispatch({type:UserActionTypes.FETCH_USERS})
+            const response = await axios.get("https://jsonplaceholder.typicode.com/users")
+            setTimeout(()=>{
+
+            },500)
+            dispatch({type:UserActionTypes.FETCH_USERS_SUCCESS,payload:response.data})
+        } catch (e) {
+            dispatch({type:UserActionTypes.FETCH_USERS_ERROR,
+                payload:"Произошла ошибка при загрузке пользователей"})
+        }
+
+    }
+}
